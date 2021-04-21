fee3500f355dc4f7e174eda10a8944bddf4d596b
Mochyy <18130004@st.hcmuaf.edu.vn>
2021-04-21 17:09:16 +0700

    basic detail page interface

---
+++ app/src/main/java/com/vientamthuong/eatsimple/detail/activity_detail.java
@@ -0,0 +1,16 @@
+package com.vientamthuong.eatsimple.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vientamthuong.eatsimple.R;
+
+public class activity_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
