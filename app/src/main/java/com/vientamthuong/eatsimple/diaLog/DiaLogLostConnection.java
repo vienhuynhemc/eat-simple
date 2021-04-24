ce8c8474437dd0c27a833cf4b2689cbd9d1947b3
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-04-24 15:34:38 +0700

    Hoàn thành bắt lỗi mạng

--- app/src/main/java/com/vientamthuong/eatsimple/diaLog/DiaLogLostConnection.java
+++ app/src/main/java/com/vientamthuong/eatsimple/diaLog/DiaLogLostConnection.java
@@ -1,23 +1,37 @@
 package com.vientamthuong.eatsimple.diaLog;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.vientamthuong.eatsimple.R;
 
 public class DiaLogLostConnection extends Dialog {
 
-    private Context context;
+    private AppCompatActivity appCompatActivity;
+    private AppCompatButton btTry;
+    private AppCompatButton btIgnore;
 
-    public DiaLogLostConnection(@NonNull Context context) {
-        super(context);
-        this.context = context;
+    public DiaLogLostConnection(@NonNull AppCompatActivity appCompatActivity) {
+        super(appCompatActivity);
+        this.appCompatActivity = appCompatActivity;
         setContentView(R.layout.dialog_lost_connection);
+        getView();
         init();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Hiện thực lại không cho làm gì cả
+    }
+
+    private void getView() {
+        btTry = findViewById(R.id.dialog_lost_connection_try);
+        btIgnore = findViewById(R.id.dialog_lost_connection_ignore);
     }
 
     private void init() {
@@ -25,5 +39,12 @@ public class DiaLogLostConnection extends Dialog {
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 
+    // GETTER AND SETTER
+    public AppCompatButton getBtTry() {
+        return btTry;
+    }
 
+    public AppCompatButton getBtIgnore() {
+        return btIgnore;
+    }
 }
---
+++ app/src/main/java/com/vientamthuong/eatsimple/loadData/LoadDataConfiguration.java
@@ -0,0 +1,8 @@
+package com.vientamthuong.eatsimple.loadData;
+
+public class LoadDataConfiguration {
+
+    public static final int IMAGE_VIEW = 0;
+    public static final int VIEW_NORMAL = 1;
+
+}
