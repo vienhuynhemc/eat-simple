1a7f3843f16d0147b9859fc1d9fae9f382e5f9cd
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-05-31 12:24:36 +0700

    DateTime và các kiểu toString cần thiết

---
+++ app/src/main/java/com/vientamthuong/eatsimple/date/DateTimeConfiguration.java
@@ -0,0 +1,8 @@
+package com.vientamthuong.eatsimple.date;
+
+public class DateTimeConfiguration {
+
+    //  1 phút bằng bao nhiêu mili
+    public static final long ONE_MINUTE_IN_MILLIS = 60000;
+
+}
