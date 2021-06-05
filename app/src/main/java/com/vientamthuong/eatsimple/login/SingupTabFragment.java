5923644176b9dadb9dd38a47baa7c8c0148c250a
nhattruongagtm <18130261@st.hcmuaf.edu.vn>
2021-06-05 23:29:01 +0700

    giả lập chức năng đăng ký

--- app/src/main/java/com/vientamthuong/eatsimple/login/SingupTabFragment.java
+++ app/src/main/java/com/vientamthuong/eatsimple/login/SingupTabFragment.java
@@ -13,26 +13,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.vientamthuong.eatsimple.R;
+import com.vientamthuong.eatsimple.loadData.VolleyPool;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SingupTabFragment extends Fragment {
     // test commit
-    EditText email, pass, repass;
+    EditText email, pass, repass, username;
     TextView notify;
     Button btnSignUp;
-    String sRePassword = "", sEmail = "", sPassword = "";
-    boolean checkPassword, checkEmail, checkLengthPass, checkSignUp;
-    float v=0;
+    String sRePassword = "", sEmail = "", sPassword = "", sUsername = "";
+    boolean checkPassword, checkEmail, checkLengthPass, checkSignUp, checkUsername;
+    String urlCheckEmail = "";
+    String urlSignUp = "";
+    float v = 0;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
-        ViewGroup root =(ViewGroup) inflater.inflate(R.layout.activity_singup_tab_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_singup_tab_fragment, container, false);
 
         mapping(root);
+
 
         pass.addTextChangedListener(new TextWatcher() {
             @Override
@@ -42,30 +57,30 @@ public class SingupTabFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    String pss = pass.getText().toString().trim();
-                    if (pss.length()<8){
-                        notify.setText("*Mật khẩu phải tối thiểu 8 kí tự!");
-                        checkLengthPass = false;
+                String pss = pass.getText().toString().trim();
+                if (pss.length() < 8) {
+                    notify.setText("*Mật khẩu phải tối thiểu 8 kí tự!");
+                    checkLengthPass = false;
+                    checkSignUp = false;
+                    notify.setTextColor(Color.RED);
+                } else {
+                    notify.setText("");
+                    checkLengthPass = true;
+                }
+                if (pss.equals("")) {
+                    notify.setText("");
+                    checkLengthPass = false;
+                }
+                if (!repass.getText().toString().equals("")) {
+                    if (!pss.equals(repass.getText().toString())) {
+                        notify.setText("*Mật khẩu không khớp");
                         notify.setTextColor(Color.RED);
+                        checkSignUp = false;
+                        checkPassword = false;
+                    } else {
+                        checkPassword = true;
                     }
-                    else{
-                        notify.setText("");
-                        checkLengthPass = true;
-                    }
-                    if (pss.equals("")){
-                        notify.setText("");
-                        checkLengthPass = false;
-                    }
-                    if(!repass.getText().toString().equals("")) {
-                        if (!pss.equals(repass.getText().toString())) {
-                            notify.setText("*Mật khẩu không khớp");
-                            notify.setTextColor(Color.RED);
-                            checkPassword = false;
-                        }
-                        else{
-                            checkPassword = true;
-                        }
-                    }
+                }
 
 
             }
@@ -83,19 +98,19 @@ public class SingupTabFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    if (!emailValidator(email.getText().toString())){
-                        notify.setText("*Vui lòng nhập đúng email!");
-                        notify.setTextColor(Color.RED);
-                        checkEmail = false;
-                    }
-                    else{
-                        notify.setText("");
-                        checkEmail = true;
-                    }
-                    if (email.getText().toString().trim().equals("")){
-                        notify.setText("");
-                        checkEmail = false;
-                    }
+                if (!emailValidator(email.getText().toString())) {
+                    notify.setText("*Vui lòng nhập đúng email!");
+                    notify.setTextColor(Color.RED);
+                    checkSignUp = false;
+                    checkEmail = false;
+                } else {
+                    notify.setText("");
+                    checkEmail = true;
+                }
+                if (email.getText().toString().trim().equals("")) {
+                    notify.setText("");
+                    checkEmail = false;
+                }
             }
 
             @Override
@@ -114,21 +129,21 @@ public class SingupTabFragment extends Fragment {
                 sRePassword = repass.getText().toString().trim();
                 sPassword = pass.getText().toString().trim();
 
-                if (pass.getText().toString().equals("") && !repass.getText().toString().equals("")){
+                if (pass.getText().toString().equals("") && !repass.getText().toString().equals("")) {
                     notify.setText("*Vui lòng nhập mật khẩu trước!");
+                    checkSignUp = false;
                     checkPassword = false;
-                }
-                else{
-                    if (!sPassword.equals(sRePassword)){
+                } else {
+                    if (!sPassword.equals(sRePassword)) {
+                        checkSignUp = false;
                         notify.setText("*Mật khẩu không khớp");
                         notify.setTextColor(Color.RED);
                         checkPassword = false;
-                    }
-                    else{
+                    } else {
                         notify.setText("");
                         checkPassword = true;
                     }
-                    if(sRePassword.equals("")){
+                    if (sRePassword.equals("")) {
                         notify.setText("");
                         checkPassword = false;
                     }
@@ -137,43 +152,144 @@ public class SingupTabFragment extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-                    sRePassword = repass.getText().toString();
+                sRePassword = repass.getText().toString();
             }
         });
 
-            btnSignUp.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    sEmail = email.getText().toString().trim();
-                    sPassword = pass.getText().toString().trim();
-                    sRePassword = repass.getText().toString().trim();
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                    if (sEmail.equals("") || sPassword.equals("") || sRePassword.equals("")) {
-                        notify.setText("*Vui lòng nhập đủ thông tin!");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String user = username.getText().toString();
+                char[] u = user.toCharArray();
+
+                if (user.equals("")) {
+                    notify.setTextColor(Color.RED);
+                    checkUsername = false;
+                    checkSignUp = false;
+                    notify.setText("*Vui lòng nhập username!");
+                } else if ((int) u[0] >= 65 && (int) u[0] <= 90 || (int) u[0] >= 97 && (int) u[0] <= 122) {
+                    if (user.length() < 12) {
+                        notify.setText("*Username phải tối thiểu 12 kí tự");
+                        notify.setTextColor(Color.RED);
                         checkSignUp = false;
-                    } else  if(checkEmail==true && checkPassword==true && checkLengthPass==true) {
-                        notify.setText("Đăng ký thành công!");
-                        checkSignUp = true;
+                        checkUsername = false;
+                    } else {
+                        notify.setText("");
+                        checkUsername = true;
                     }
+                } else {
+                    notify.setText("*Username phải bắt đầu bằng một kí tự chữ!");
+                    notify.setTextColor(Color.RED);
+                    checkSignUp = false;
+                    checkUsername = false;
+                }
+            }
 
-                    if (checkSignUp){
-                        notify.setTextColor(Color.GREEN);
-                    }
+            @Override
+            public void afterTextChanged(Editable s) {
+                String user = username.getText().toString();
+                if (user.equals("")) {
+                    notify.setTextColor(Color.RED);
+                    notify.setText("");
+                    checkUsername = false;
+                }
+            }
+        });
 
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sUsername = username.getText().toString().trim();
+                sEmail = email.getText().toString().trim();
+                sPassword = pass.getText().toString().trim();
+                sRePassword = repass.getText().toString().trim();
+
+                if (sEmail.equals("") || sPassword.equals("") || sRePassword.equals("") || sUsername.equals("")) {
+                    notify.setText("*Vui lòng nhập đủ thông tin!");
+                    checkSignUp = false;
+                } else if (checkEmail == true && checkPassword == true && checkLengthPass == true && checkUsername == true) {
+                    notify.setText("Đăng ký thành công!");
+                    checkSignUp = true;
                 }
 
-            });
+                if (checkSignUp) {
+                    notify.setTextColor(Color.GREEN);
+                }
+            }
 
+        });
 
 
         return root;
     }
-    public void mapping(View root){
+
+    public void checkEmail(String email) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlCheckEmail,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+            @Nullable
+            @org.jetbrains.annotations.Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("email", email);
+                return params;
+            }
+        };
+        VolleyPool.getInstance(getContext()).addRequest(stringRequest);
+
+    }
+
+    public void signUp(String email, String password) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlSignUp,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+            @Nullable
+            @org.jetbrains.annotations.Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        VolleyPool.getInstance(getContext()).addRequest(stringRequest);
+
+    }
+
+    public void mapping(View root) {
         email = root.findViewById(R.id.email_signUp);
         pass = root.findViewById(R.id.password_signUp);
         repass = root.findViewById(R.id.repassword_signUp);
         btnSignUp = root.findViewById(R.id.button_singup);
         notify = root.findViewById(R.id.notify_signUp);
+        username = root.findViewById(R.id.username_signUp);
 
         email.setTranslationX(0);
         pass.setTranslationX(0);
@@ -190,8 +306,8 @@ public class SingupTabFragment extends Fragment {
         repass.animate().translationY(100).alpha(1).setDuration(800).setStartDelay(500).start();
         btnSignUp.animate().translationY(100).alpha(1).setDuration(800).setStartDelay(700).start();
     }
-    public boolean emailValidator(String email)
-    {
+
+    public boolean emailValidator(String email) {
         Pattern pattern;
         Matcher matcher;
         final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
